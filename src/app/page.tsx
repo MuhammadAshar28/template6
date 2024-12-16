d76194f67d41a7a9ef907f937b8fc8c6a741e449
Muhammad Ashar <165711594+MuhammadAshar28@users.noreply.github.com>
2024-12-16 08:49:11 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import First from "@/components/sections/First";
 import Hero from "@/components/sections/Hero";
 import Second from "@/components/sections/Second";
-import Image from "next/image"
 
 export default function Home() {
   return (
